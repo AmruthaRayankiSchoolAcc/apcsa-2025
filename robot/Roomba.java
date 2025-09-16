37021258ccfaacacba9f9ad7d7daece1aca57f4b
AmruthaRayankiSchoolAcc <ar8602@pleasantonusd.net>
2025-09-16 06:37:59 +0000

    Goal #3

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -4,6 +4,8 @@ import kareltherobot.*;
 
 public class Roomba implements Directions {
 
+	int totalBeepers = 0;
+
 	// Main method to make this self-contained
 	public static void main(String[] args) {
 		// LEAVE THIS ALONE!!!!!!
@@ -15,11 +17,34 @@ public class Roomba implements Directions {
 
 	}
 
-	// declared here so it is visible in all the methods!
-	//private Robot robot;
+	// function for cleaning the row
+	public void cleanRow(Robot r) {
+		while(r.frontIsClear()) {
+			r.move();
+			while(r.nextToABeeper()) {
+				r.pickBeeper();
+				totalBeepers++;
+			}
+		}
+	}
 
-	// You will need to add many variables!!
-
+	// function for turning left to move to next row 
+	public void turnByLeft(Robot r) {
+		r.turnLeft();
+		r.move();
+		r.turnLeft();
+	}
+	
+	// function for turning right to move to next row
+	public void turnByRight(Robot r) {
+		r.turnLeft();
+		r.turnLeft();
+		r.turnLeft();
+		r.move();
+		r.turnLeft();
+		r.turnLeft();
+		r.turnLeft();
+	}
 
 	public int cleanRoom(String worldName, int startX, int startY) {
 
@@ -32,81 +57,23 @@ public class Roomba implements Directions {
 		World.setVisible(true);
 		World.setDelay(10);
 
-		int totalBeepers = 0; // Need to move this somewhere else.
-
 		/** This section will have all the logic that takes the Robot to every location
 		 * and cleans up all piles of beepers. Think about ways you can break this
 		 * large, complex task into smaller, easier to solve problems.
 		 */
+
 		// robot start moving
 		robot.turnLeft(); 
-		// repeating code: function to clear the row
-		while(robot.frontIsClear()) {
-			robot.move();
-			while(robot.nextToABeeper()) {
-				robot.pickBeeper();
-				totalBeepers++;
-			}
-		}
-		// in-between while loop, the code for robot to move to next row when turning left
-		robot.turnLeft();
-		robot.move();
-		robot.turnLeft();
-		// repeating code: function to clear the row
-		while(robot.frontIsClear()) {
-			robot.move();
-			while(robot.nextToABeeper()) {
-				robot.pickBeeper();
-				totalBeepers++;
-			}
-		}
-		// code for robot to move to next row when turning right
-		robot.turnLeft();
-		robot.turnLeft();
-		robot.turnLeft();
-		robot.move();
-		robot.turnLeft();
-		robot.turnLeft();
-		robot.turnLeft();
-		// repeating code: function to clear the row
-		while(robot.frontIsClear()) {
-			robot.move();
-			while(robot.nextToABeeper()) {
-				robot.pickBeeper();
-				totalBeepers++;
-			}
-		}
-		// code for robot to move to next row moving left
-		robot.turnLeft();
-		robot.move();
-		robot.turnLeft();
-		// repeating code: function to clear the row
-		while(robot.frontIsClear()) {
-			robot.move();
-			while(robot.nextToABeeper()) {
-				robot.pickBeeper();
-				totalBeepers++;
-			}
-		}
-		// code for robot to move to next row when turning right
-		robot.turnLeft();
-		robot.turnLeft();
-		robot.turnLeft();
-		robot.move();
-		robot.turnLeft();
-		robot.turnLeft();
-		robot.turnLeft();
-		// repeating code: function to clear the row
-		while(robot.frontIsClear()) {
-			robot.move();
-			while(robot.nextToABeeper()) {
-				robot.pickBeeper();
-				totalBeepers++;
-			}
-		}
-
-
-
+		cleanRow(robot);
+		turnByLeft(robot);
+		cleanRow(robot);
+		turnByRight(robot);
+		cleanRow(robot);
+		turnByLeft(robot);
+		cleanRow(robot);
+		turnByRight(robot);
+		cleanRow(robot);
+		
         // This method should return the total number of beepers cleaned up.
 		return totalBeepers;
 	}
