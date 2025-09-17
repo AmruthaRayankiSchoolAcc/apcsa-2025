c4fd87da5cbef458dacef25fbc71aaa4e3cbd2b4
AmruthaRayankiSchoolAcc <ar8602@pleasantonusd.net>
2025-09-17 03:29:30 +0000

    Goal #5 Part 2: Applicable to any world

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -4,7 +4,8 @@ import kareltherobot.*;
 
 public class Roomba implements Directions {
 
-	int squareCount = 0;
+	// you start with 1 for squareCount because when you start the program robot is inside first square
+	int squareCount = 1;
 	int totalBeepers = 0;
 	int totalPiles = 0;
 	int maximumBeepersPerPile = 0;
@@ -12,16 +13,21 @@ public class Roomba implements Directions {
 	// largestPileLocation (street and avenue)
 	int largestPileStreet = 0;
 	int largestPileAvenue = 0;
+	boolean needToCleanMore = true;
 
 	// Main method to make this self-contained
 	public static void main(String[] args) {
-		// LEAVE THIS ALONE!!!!!!
-		String worldName = "robot/basicRoom.wld";
+		// String worldName = "robot/basicRoom.wld";
+		// String worldName = "robot/TestWorld-1.wld";
+		// String worldName = "robot/TestWorld-2.wld";
+		String worldName = "robot/finalTestWorld2024.wld";
 
 		Roomba cleaner = new Roomba();
-		int totalBeepers = cleaner.cleanRoom(worldName, 7, 6);
+		// int totalBeepers = cleaner.cleanRoom(worldName, 7, 6); for basicRoom
+		// int totalBeepers = cleaner.cleanRoom(worldName, 25 , 11); for TestWorld-1
+		// int totalBeepers = cleaner.cleanRoom(worldName, 5, 6); for TestWorld-2
+		int totalBeepers = cleaner.cleanRoom(worldName, 26, 101); // coordinates for final world
 		System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
-
 	}
 
 	// function for cleaning the row
@@ -52,7 +58,12 @@ public class Roomba implements Directions {
 	// function for turning left to move to next row 
 	public void turnByLeft(Robot r) {
 		r.turnLeft();
-		r.move();
+		if (r.frontIsClear()) {
+			r.move();
+			squareCount++;
+		} else {
+			needToCleanMore = false;
+		}
 		r.turnLeft();
 	}
 	
@@ -61,7 +72,12 @@ public class Roomba implements Directions {
 		r.turnLeft();
 		r.turnLeft();
 		r.turnLeft();
-		r.move();
+		if (r.frontIsClear()) {
+			r.move();
+			squareCount++;
+		} else {
+			needToCleanMore = false;
+		}
 		r.turnLeft();
 		r.turnLeft();
 		r.turnLeft();
@@ -69,34 +85,30 @@ public class Roomba implements Directions {
 
 	public int cleanRoom(String worldName, int startX, int startY) {
 
-		// A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
         // Make sure it starts at startX and startY location.
-		Robot robot = new Robot(startX, startY, South, 0);
+		Robot robot = new Robot(startX, startY, East, 0);
 
 		//creates the world for the robot to clean
 		World.readWorld(worldName);
 		World.setVisible(true);
-		World.setDelay(10);
+		World.setDelay(0);
 
-		/** This section will have all the logic that takes the Robot to every location
-		 * and cleans up all piles of beepers. Think about ways you can break this
-		 * large, complex task into smaller, easier to solve problems.
-		 */
-
-		// robot start moving
-		robot.turnLeft(); 
-		cleanRow(robot);
-		turnByLeft(robot);
-		cleanRow(robot);
-		turnByRight(robot);
-		cleanRow(robot);
-		turnByLeft(robot);
-		cleanRow(robot);
-		turnByRight(robot);
-		cleanRow(robot);
+		// main code for robot to move through the world
+		while (needToCleanMore) {
+			if (robot.frontIsClear()) {
+				cleanRow(robot);
+			}
+			if (robot.facingEast()) {
+				turnByLeft(robot);
+			}
+			else { // robot.facingWest()
+				turnByRight(robot);
+			}
+		}
 
 		System.out.println("Total number of piles: " + totalPiles);
 		System.out.println(largestPile + " is the largest pile in the total number of piles " + totalPiles);
+		System.out.println("Largest pile has " + maximumBeepersPerPile + " beepers");
 		System.out.println("Largest pile location is " + "street " + largestPileStreet + " avenue " + largestPileAvenue);
 		System.out.println("Area is " + squareCount);
         // This method should return the total number of beepers cleaned up.
